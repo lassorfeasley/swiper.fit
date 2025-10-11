03e614a4dc9fbbe1cb749a37aed136200714681b
Lassor Feasley <Feasley@Lassor.com>
2025-10-10 20:17:24 -0400

    Refactor button components and enhance PublicRoutine functionality: update button styles for improved aesthetics and usability, integrate SwiperButton for routine saving options, and implement dialog for adding routines to user accounts. Adjust layout for better user experience.

--- src/components/molecules/swiper-button.jsx
+++ src/components/molecules/swiper-button.jsx
@@ -3,13 +3,13 @@ import { Button } from "@/components/atoms/button";
 import { cva } from "class-variance-authority";
 import { cn } from "@/lib/utils";
 
-const swiperButtonVariants = cva("text-base font-normal h-[52px] rounded-none border border-neutral-300 focus-visible:outline-none focus-visible:ring-0 focus-visible:ring-offset-0", {
+const swiperButtonVariants = cva("text-base font-medium font-['Be_Vietnam_Pro'] leading-tight h-12 rounded-xl border border-neutral-300 px-4 py-2 focus-visible:outline-none focus-visible:ring-0 focus-visible:ring-offset-0", {
   variants: {
     variant: {
-      default: "bg-slate-600 text-white hover:bg-slate-500",
+      default: "bg-neutral-neutral-600 text-white hover:bg-neutral-neutral-500",
       destructive: "!bg-red-300 text-neutral-700 hover:!bg-red-400",
       outline:
-        "border-neutral-300 bg-neutral-50 text-slate-600 hover:bg-neutral-100",
+        "border-neutral-300 bg-white text-neutral-neutral-600 hover:bg-neutral-neutral-100",
       "primary-action": "w-full bg-neutral-neutral-700 text-white hover:bg-neutral-neutral-600 rounded-lg outline outline-1 outline-offset-[-1px] outline-neutral-neutral-300 px-3 py-0 text-left text-xs font-bold font-['Be_Vietnam_Pro'] uppercase leading-5 tracking-wide overflow-hidden h-12 min-h-[48px] justify-start items-center",
     },
   },
