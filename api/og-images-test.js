040624b1703fed35d40f719cbaa444a7083f9c3b
Lassor <feasley@lassor.com>
2025-10-24 15:25:32 -0400

    Remove test endpoint to stay under serverless function limit

--- api/og-images-test.js
+++
@@ -1,8 +0,0 @@
-export default async function handler(req, res) {
-  res.setHeader('Content-Type', 'application/json');
-  return res.status(200).json({
-    success: true,
-    message: 'OG images endpoint working',
-    query: req.query
-  });
-}
